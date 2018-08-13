4e37a2ea3955ef8945a8dba4df6e8e5f71b0fa20
rickymax <rickyomax@gmail.com>
2018-08-13 09:44:58 +0000

    add generate-bl.sh for bosh login command

---
+++ scripts/generate-bl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "=================================================="
+echo " Generate BOSH Login Command"
+echo "=================================================="
+read -r -p "Enter BOSH aliased environment name (default 10.0.0.100): " BOSH_ENDPOINT
+read -r -p "Enter BOSH aliased environment name (default spacex): " BOSH_ENV
+
+if [[ -z "$BOSH_ENDPOINT" ]]; then
+    BOSH_ENDPOINT=10.0.0.100
+fi
+
+if [[ -z "$BOSH_ENV" ]]; then
+    BOSH_ENV=spacex
+fi
+
+cat << EOF >> bl
+#!/bin/bash
+bosh -e $BOSH_ENDPOINT alias-env $BOSH_ENV --ca-cert <(bosh int /opt/spacex/creds.yml --path /director_ssl/ca)
+bosh -e $BOSH_ENV login < /usr/local/bin/director_credentails
+EOF
+
+cat << EOF >> director_credentails
+admin
+`bosh int /opt/spacex/creds.yml --path /admin_password`
+EOF
+
+chmod +x bl
+
+mv bl /usr/local/bin
+mv director_credentails-t /usr/local/bin
+
+exit 0
