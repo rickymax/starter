100e3d2961a7ca8d83e1b32ecfe7e6978508d5b3
rickymax <rickyomax@gmail.com>
2018-08-27 02:52:45 +0000

    do not git clone if workspaces not found

--- scripts/multiple-git-clone.sh
+++ scripts/multiple-git-clone.sh
@@ -4,11 +4,14 @@ WORKSPACES=/opt/spacex/workspaces
 readarray array <<< $( cat "$@" )
 
 if [ -d $WORKSPACES ]; then
-    cd $WORKSPACES/tmp
+    cd $WORKSPACES
+    for element in ${array[@]}
+    do
+      echo "clonning $element"
+      git clone $element
+    done
+    exit 0
+else
+    echo "$WORKSPACES not found"
+    exit 1
 fi
-
-for element in ${array[@]}
-do
-  echo "clonning $element"
-  git clone $element
-done
