e736af0a024d237da77803f41c1dbcfe5a88ea12
rickymax <rickyomax@gmail.com>
2019-04-16 05:31:18 +0000

    update generate-bl.sh: add two parameters

--- scripts/generate-bl.sh
+++ scripts/generate-bl.sh
@@ -2,15 +2,21 @@
 echo "=================================================="
 echo " Generate BOSH Login Command"
 echo "=================================================="
-read -r -p "Enter BOSH aliased environment name (default 10.0.0.100): " BOSH_ENDPOINT
-read -r -p "Enter BOSH aliased environment name (default spacex): " BOSH_ENV
 
-if [[ -z "$BOSH_ENDPOINT" ]]; then
-    BOSH_ENDPOINT=10.0.0.100
-fi
+if [[ -n "$1" && -n "$2" ]]; then
+    BOSH_ENDPOINT=$1
+    BOSH_ENV=$2
+else
+    read -r -p "Enter BOSH aliased environment name (default 10.0.0.100): " BOSH_ENDPOINT
+    read -r -p "Enter BOSH aliased environment name (default spacex): " BOSH_ENV
 
-if [[ -z "$BOSH_ENV" ]]; then
-    BOSH_ENV=spacex
+    if [[ -z "$BOSH_ENDPOINT" ]]; then
+        BOSH_ENDPOINT=10.0.0.100
+    fi
+
+    if [[ -z "$BOSH_ENV" ]]; then
+        BOSH_ENV=spacex
+    fi
 fi
 
 cat << EOF >> bl
